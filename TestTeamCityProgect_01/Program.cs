7ce63911a5a8a28c71231f6867742c4a986e8a94
andrei_zayats <andrei_zayats@outlook.com>
2019-08-10 10:33:03 +0300

    add dummy test

---
+++ TestTeamCityProgect_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestTeamCityProgect_01
+{
+    public class NUnitTest
+    {
+        [Test]
+        public void DataTimeTest()
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(TimeSpan.FromMinutes(3));
+            var endTime = DateTime.Now;
+            startTime.Should().BeBefore(endTime);
+        }
+    }
+}

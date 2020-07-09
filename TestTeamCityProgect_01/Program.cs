998b1bab76ad11c96d52b547471709f1580854db
andrei_zayats <andrei_zayats@outlook.com>
2020-07-09 23:39:15 +0300

    Three types of tests are added.

--- TestTeamCityProgect_01/Program.cs
+++ TestTeamCityProgect_01/Program.cs
@@ -15,5 +15,35 @@ namespace TestTeamCityProgect_01
             var endTime = DateTime.Now;
             startTime.Should().BeBefore(endTime);
         }
+
+        [Test]
+        public void DataTime_PassTest()
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine("This is test is passed!");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            var endTime = DateTime.Now;
+            startTime.Should().BeBefore(endTime);
+        }
+
+        [Test]
+        public void DataTime_WarningTest()
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine("This is test is worried!");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            var endTime = DateTime.Now;
+            Warn.If(startTime < endTime);
+        }
+
+        [Test]
+        public void DataTime_FailTest()
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine("This is test is failed!");
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            var endTime = DateTime.Now;
+            startTime.Should().BeAfter(endTime);
+        }
     }
 }
